4c67b37234b4ebe0804015f589e4e34405070861
Sebastian <mrseb10@gmail.com>
2024-06-03 13:08:13 +0200

    Routes, login/register forms templates and styles

---
+++ UI/src/app/Auth/components/auth-layout/auth-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {RouterOutlet} from "@angular/router";
+import {TitleComponent} from "../../../Shared/title/title.component";
+
+@Component({
+  selector: 'app-auth-layout',
+  standalone: true,
+  imports: [RouterOutlet, TitleComponent],
+  templateUrl: './auth-layout.component.html',
+  styleUrl: './auth-layout.component.css',
+})
+export class AuthLayoutComponent {}
