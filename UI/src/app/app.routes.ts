173b18edfc7737d0aab2c4a66b9e20c511b5bb16
Sebastian <mrseb10@gmail.com>
2024-06-03 13:08:02 +0200

    Routes, login/register forms templates and styles

--- UI/src/app/app.routes.ts
+++ UI/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import {DashboardLayoutComponent} from "./Dashboard/components/dashboard-layout/dashboard-layout.component";
+import {DashboardComponent} from "./Dashboard/components/dashboard/dashboard.component";
+import {AuthLayoutComponent} from "./Auth/components/auth-layout/auth-layout.component";
+import {LoginComponent} from "./Auth/components/auth-layout/login/login.component";
+import {RegisterComponent} from "./Auth/components/auth-layout/register/register.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: DashboardLayoutComponent, children: [
+    {path: '', component: DashboardComponent},
+    ]},
+  {path: "", component: AuthLayoutComponent, children: [
+      {path: "login", component: LoginComponent},
+      {path: "register", component: RegisterComponent},
+    ]}
+];
--- UI/tailwind.config.js
+++ UI/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "midnight-blue": "#0b2145",
+        "steel-blue": "#3661a0",
+        "royal-blue": "#3a82e4",
+        "royal-blue-hover": "#5da0fd",
+      },
+    },
   },
   plugins: [],
-}
-
+};
