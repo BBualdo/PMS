e6f6c28269a2aa3c9d1846214cc8f29067c442fb
Sebastian <mrseb10@gmail.com>
2024-06-06 10:54:46 +0200

    Angular: Removed unnecessary test and css files

--- UI/src/app/Auth/components/auth-layout/register/register.component.ts
+++ UI/src/app/Auth/components/auth-layout/register/register.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [RouterLink],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css',
 })
 export class RegisterComponent {}
--- UI/src/app/Dashboard/components/dashboard-layout/dashboard-layout.component.ts
+++ UI/src/app/Dashboard/components/dashboard-layout/dashboard-layout.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
-import {RouterOutlet} from "@angular/router";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
-  imports: [
-    RouterOutlet
-  ],
+  imports: [RouterOutlet],
   templateUrl: './dashboard-layout.component.html',
-  styleUrl: './dashboard-layout.component.css'
 })
-export class DashboardLayoutComponent {
-
-}
+export class DashboardLayoutComponent {}
