ccfea819f70ae32a1a07ad7517eeb350ea1e853f
Sebastian <mrseb10@gmail.com>
2024-06-06 11:42:26 +0200

    Register form group and validators (with custom ones)

--- UI/src/app/Auth/components/auth-layout/register/register.component.ts
+++ UI/src/app/Auth/components/auth-layout/register/register.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { emailValidator } from '../../../../../validators/email.validator';
+import { passwordValidator } from '../../../../../validators/password.validator';
+import { matchPasswordValidator } from '../../../../../validators/match-password.validator';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +11,30 @@ import { RouterLink } from '@angular/router';
   imports: [RouterLink],
   templateUrl: './register.component.html',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  registerForm: FormGroup = new FormGroup(
+    {
+      firstName: new FormControl<string>('', [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(50),
+      ]),
+      lastName: new FormControl<string>('', [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(50),
+      ]),
+      email: new FormControl<string>('', [Validators.required, emailValidator]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        passwordValidator,
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    },
+    [matchPasswordValidator],
+  );
+}
