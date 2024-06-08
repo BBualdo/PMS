7873b4859dfd5f13d22c4c55391989e326d8d644
Sebastian <mrseb10@gmail.com>
2024-06-08 20:27:03 +0200

    Sidebar links and styles

--- UI/src/app/Dashboard/components/shared/sidebar/sidebar.component.ts
+++ UI/src/app/Dashboard/components/shared/sidebar/sidebar.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { Router } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'sidebar',
   standalone: true,
-  imports: [],
+  imports: [MatIcon, NgClass],
   templateUrl: './sidebar.component.html',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  router = inject(Router);
+  sidebarLinks = [
+    { title: 'Dashboard', slug: '/', icon: 'home' },
+    { title: 'Products', slug: '/products', icon: 'warehouse' },
+    { title: 'Users', slug: '/users', icon: 'supervisor_account' },
+  ];
+  currentUrl = this.router.url;
+}
