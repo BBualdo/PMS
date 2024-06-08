a0f9567ea0c59e340b71dcdb7068dd28b752d714
Sebastian <mrseb10@gmail.com>
2024-06-08 20:00:30 +0200

    Split sidebar and navbar to components

--- UI/src/app/Dashboard/components/dashboard-layout/dashboard-layout.component.ts
+++ UI/src/app/Dashboard/components/dashboard-layout/dashboard-layout.component.ts
@@ -1,22 +1,26 @@
 import { Component, inject } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { AuthService } from '../../../../services/auth.service';
 import { AsyncPipe } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
 import { TitleComponent } from '../../../Shared/title/title.component';
+import { SidebarComponent } from '../shared/sidebar/sidebar.component';
+import { NavbarComponent } from '../shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
-  imports: [RouterOutlet, AsyncPipe, MatIcon, TitleComponent],
+  imports: [
+    RouterOutlet,
+    AsyncPipe,
+    MatIcon,
+    TitleComponent,
+    SidebarComponent,
+    NavbarComponent,
+  ],
   templateUrl: './dashboard-layout.component.html',
 })
 export class DashboardLayoutComponent {
   private authService = inject(AuthService);
-  private router = inject(Router);
   currentUser$ = this.authService.currentUser$;
-
-  logout() {
-    this.authService.logout().subscribe(() => this.router.navigate(['/login']));
-  }
 }
--- UI/src/app/Dashboard/components/dashboard/dashboard.component.ts
+++ UI/src/app/Dashboard/components/dashboard/dashboard.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { AuthService } from '../../../../services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './dashboard.component.html',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  private authService = inject(AuthService);
+  currentUser$ = this.authService.currentUser$;
+}
