253baab827c4ad65374d436f495c47f584e2e029
Sebastian <mrseb10@gmail.com>
2024-06-09 02:15:51 +0200

    Created AccountEmailHelper to reuse it when Admin creates or updates user

--- API/Services/UsersService.cs
+++ API/Services/UsersService.cs
@@ -1,12 +1,14 @@
-﻿using API.Services.DTOs;
+﻿using API.Helpers;
+using API.Services.DTOs;
 using Data.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Services;
 
-public class UsersService(UserManager<User> userManager) : IUsersService
+public class UsersService(UserManager<User> userManager, AccountEmailHelper emailHelper) : IUsersService
 {
+    private readonly AccountEmailHelper _emailHelper = emailHelper;
     private readonly UserManager<User> _userManager = userManager;
 
     public async Task<PaginatedUsers> GetUsersAsync(int page, int pageSize)
@@ -40,6 +42,9 @@ public class UsersService(UserManager<User> userManager) : IUsersService
 
         var result = await _userManager.CreateAsync(user, model.Password);
 
+        if (result.Succeeded)
+            await _emailHelper.SendConfirmationEmailAsync(user);
+
         return result.Succeeded;
     }
 
@@ -52,11 +57,16 @@ public class UsersService(UserManager<User> userManager) : IUsersService
     public async Task<bool> UpdateUserAsync(string id, UpdateUserModel model)
     {
         var user = await _userManager.FindByIdAsync(id);
-        if (user == null) return false;
+        if (user == null)
+            return false;
+
+        if (user.Email != model.Email)
+            await _emailHelper.SendConfirmationEmailAsync(user);
 
         user.FirstName = model.FirstName;
         user.LastName = model.LastName;
         user.Email = model.Email;
+        user.UserName = model.Email;
 
         var result = await _userManager.UpdateAsync(user);
 
