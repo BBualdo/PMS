8bc53215a7f35768b253158b74685e15c0e1e458
Sebastian <mrseb10@gmail.com>
2024-06-09 02:50:32 +0200

    Products Service with get method, products table test template

--- UI/src/app/Dashboard/components/products/products.component.ts
+++ UI/src/app/Dashboard/components/products/products.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductsService } from '../../../../services/products.service';
+import { AsyncPipe, formatDate } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, MatIcon],
   templateUrl: './products.component.html',
 })
-export class ProductsComponent {}
+export class ProductsComponent implements OnInit {
+  private productsService = inject(ProductsService);
+
+  products$ = this.productsService.products$;
+
+  ngOnInit() {
+    this.productsService.getProducts().subscribe((value) => console.log(value));
+  }
+
+  protected readonly formatDate = formatDate;
+}
---
+++ UI/src/services/products.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { ErrorsService } from './errors.service';
+import { LoadingService } from './loading.service';
+import {
+  BehaviorSubject,
+  catchError,
+  finalize,
+  Observable,
+  of,
+  tap,
+} from 'rxjs';
+import { url } from '../config/config';
+import { PaginatedProducts } from '../models/PaginatedProducts';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductsService {
+  private productsSubject: BehaviorSubject<
+    PaginatedProducts | null | undefined
+  > = new BehaviorSubject<PaginatedProducts | null | undefined>(undefined);
+  products$ = this.productsSubject.asObservable();
+
+  constructor(
+    private http: HttpClient,
+    private errorsService: ErrorsService,
+    private loadingService: LoadingService,
+  ) {}
+
+  getProducts(
+    page: number = 1,
+    pageSize: number = 5,
+  ): Observable<PaginatedProducts> {
+    this.loadingService.startLoading();
+    return this.http
+      .get<PaginatedProducts>(
+        url + `Products/?page=${page}&pageSize=${pageSize}`,
+      )
+      .pipe(
+        tap((products) => this.productsSubject.next(products)),
+        catchError((error) => of(this.handleErrors(error))),
+        finalize(() => this.loadingService.stopLoading()),
+      );
+  }
+
+  private handleErrors(error: HttpErrorResponse): any {
+    switch (error.status) {
+    }
+
+    console.log(error);
+  }
+}
